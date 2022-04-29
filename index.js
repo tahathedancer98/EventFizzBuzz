fe3dd9172f51c94a3e28e689ecc07d1254c10c5a
tahathedancer98 <bookingtaha98@gmail.com>
2022-04-29 12:36:30 +0200

    Add Event listener

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+const EventEmitter = require('events');
+const myEmitter = new EventEmitter();
+
+var n = readline()
+myEmitter.on('FizzBuzz', FizzBuzz(n));
+
 function FizzBuzz(nombre){
     if(nombre % 5 == 0) console.log("Buzz")
     else if(nombre % 5 == 0 && nombre % 3 == 0) console.log("FizzBuzz")
     else if(nombre % 3 == 0) console.log("Fizz")
     else console.log(nombre)
 }
+
